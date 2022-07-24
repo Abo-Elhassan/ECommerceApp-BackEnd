00d6b79e09a299f9273d3ea12d90095ded88ae20
YussufHassan <youssefhassan711@gmail.com>
2022-07-24 23:26:11 +0200

    V_Commit

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -8,6 +8,8 @@ namespace Core.Entities
         public Guid Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DataType(DataType.MultilineText)]
+
         public string Description { get; set; }
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
